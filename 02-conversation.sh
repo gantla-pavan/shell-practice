090b3055ade574ff77cd080ddd55773ed4bcc0de
gantlapavan <gantlapavan1705@gmail.com>
2025-09-19 17:05:51 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: Hi suresh, how are you?"
+echo:"sursh:: hello Ramesh, I am fine. How about you?"
+echo:"ramesh:: what are you learning recently?"
+echo:"suresh:: I am learning shell scripting ramesh"
