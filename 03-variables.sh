0710d5ed432e024aad30bccc2f393d7f533ef7b3
gantlapavan <gantlapavan1705@gmail.com>
2025-09-19 17:47:07 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 PERSON1=Sachin
 PERSON2=Rahul
 
-echo "$PERSON1:: Hi PERSON2, how are you?"
-echo:"PERSON2:: hello $PERSON1, I am fine. How about you?"
-echo:"$PERSON1:: what are you learning recently?"
-echo:"PERSON2:: I am learning shell scripting $PERSON1"
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, I am fine. How about you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
