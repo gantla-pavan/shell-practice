fd30e59ffb103159893350229e07d53efee02363
gantlapavan <gantlapavan1705@gmail.com>
2025-09-23 17:28:56 +0530

    some message

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 echo "please enter the number:"
 read NUMBER
 
-if[ $((NUMBER % 2)) -eq 0 ]; then
+if[ $(($NUMBER % 2)) -eq 0 ]; then
     echo "GIVEN number $NUMBER is EVEN"
 else
     echo "GIVEN number $NUMBER is ODD"
