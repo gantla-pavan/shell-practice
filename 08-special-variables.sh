1599194c8ce7a00455b63e9b0e2e80e14d4af283
gantlapavan <gantlapavan1705@gmail.com>
2025-09-23 14:40:04 +0530

    some message

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -6,5 +6,5 @@ echo "all variables passed to the script: $*"
 echo "Script Name: $0"
 echo "Current directory: $PWD"
 echo "Who is working on this: $USER"
-echo :Home directory of the user: $HOME
-echo :PID of the current script: $$
+echo :"Home directory of the user: $HOME"
+echo :"PID of the current script: $$""
