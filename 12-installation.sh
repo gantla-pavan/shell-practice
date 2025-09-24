0fa0db728699676c1155816000d7471c2aa887ab
gantlapavan <gantlapavan1705@gmail.com>
2025-09-24 14:38:04 +0530

    some message

--- 12-installation.sh
+++ 12-installation.sh
@@ -3,13 +3,13 @@
 USERID=$(id -u)
 
 
-if[$USERID -ne 0]; then
-    echo "ERROR:: Please run this script with the root privilege"
+if[ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with the root privelege"
 fi 
 
 dnf install mysql -y
  
- if [$? -ne 0]; then
+ if [ $? -ne 0 ]; then
     echo "ERROR:: Installing MYSQL is failure"
     else
     echo "Installing MYSQL is success"
