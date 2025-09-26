b1f7e35c2940f4f95ecdf83ecb9f844b8875359d
gantlapavan <gantlapavan1705@gmail.com>
2025-09-26 17:22:52 +0530

    some message

--- 18-loops.sh
+++ 18-loops.sh
@@ -11,7 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p "$LOGS_FOLDER"
- echo "script started executed at $(date)" | tee -a $LOGS_FILE
+ echo "script started executed at $(date)" | tee -a $LOGS_FILE 
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with the root privelege"
@@ -33,5 +33,13 @@ fi
 
 for package in $@
 do
-    echo "package is: $package"
+    #check package is installed or not
+    dnf installed $package &>>$LOGS_FILE
+
+    if [ $? -ne 0 ]; then
+        dnf install $package -y
+        VALIDATE $? "$package"
+    else
+        echo -e "$package is already installed ...$Y SKIPPING $N""
+
 done
