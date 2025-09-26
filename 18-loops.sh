6931db34e2c968f901efdc7466c05aa2334de096
gantlapavan <gantlapavan1705@gmail.com>
2025-09-26 17:12:34 +0530

    some message

---
+++ 18-loops.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+    R="\e[31m"
+    G="\e[32m"
+    Y="\e[33m"
+    N="\e[34m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FOLDER"
+ echo "script started executed at $(date)" | tee -a $LOGS_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with the root privelege"
+     exit 1 # fialure is other than 0
+fi
+
+VALIDATE(){
+ if [ $1 -ne 0 ]; then
+    echo -e "Installing $2...$R is FAILURE $N" | tee -a $LOGS_FILE
+
+        exit 1
+else
+    echo -e "Installing $2...$G is SUCCESS $N" | tee -a $LOGS_FILE
+
+fi
+}
+
+#$@
+
+for package in $@
+do
+    echo "package is: $package"
+done
