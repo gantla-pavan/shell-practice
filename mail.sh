b5159854391d24f7455ff597f85ee2a3a697af8a
gantlapavan <gantlapavan1705@gmail.com>
2025-09-29 19:34:44 +0530

    some message

--- mail.sh
+++ mail.sh
@@ -3,11 +3,11 @@ TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 MESSAGE_BODY=$4
-FORMATED_BODY=$(printf '%s\n'$MESSAGE_BODY | sed -e "s/'/'\\\\''/g; ls/^/'/; \$s/\$/'/")
+FORMATED_BODY=$(printf '%s\n'"$MESSAGE_BODY" | sed -e "s/'/'\\\\''/g; ls/^/'/; \$s/\$/'/")
 IP_ADDRESS=$5
 To_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALER_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALER_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATED_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
