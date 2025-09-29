455a757bfbafb7725a23134edf0e8403a9312b78
gantlapavan <gantlapavan1705@gmail.com>
2025-09-29 18:59:51 +0530

    some message

---
+++ mail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TO_ADDRESS=$1
+SUBJECT=$2
+ALERT_TYPE=$3
+MESSAGE_BODY=$4
+IP_ADDRESS=$5
+To_TEAM=$6
+
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALER_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
+
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_BODY"
+} | msmtp "$TO_ADDRESS"
