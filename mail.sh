cb846acac7f0e46c46baf339ca03a97bb82f3f31
gantlapavan <gantlapavan1705@gmail.com>
2025-09-29 19:16:40 +0530

    some message

--- mail.sh
+++ mail.sh
@@ -2,7 +2,7 @@
 TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
-MESSAGE_BODY=$(echo $4 | sed -e "s/'/'\\\\''/g; ls/^/'/; \$s/\$/'/")
+MESSAGE_BODY=$(printf '%s\n' | sed -e "s/'/'\\\\''/g; ls/^/'/; \$s/\$/'/")
 IP_ADDRESS=$5
 To_TEAM=$6
 
